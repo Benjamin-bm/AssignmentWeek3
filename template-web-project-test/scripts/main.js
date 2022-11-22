318b6f728fc1612c80ca8a022c29f1273387318c
Benjamin-bm <112786874+Benjamin-bm@users.noreply.github.com>
2022-11-22 13:21:25 -0500

    .

--- template-web-project-test/scripts/main.js
+++ template-web-project-test/scripts/main.js
@@ -3,3 +3,11 @@ document.querySelector('body').appendChild(newdiv);
 console.log('HI IM WORKING');
 
 const obj = { name: 'omar' };
+
+document.querySelector('.container2').appendChild(div);
+
+div.classList.add('btn-click');
+div.classList.remove('btn');
+
+div.innerText = 'dude whats up';
+document.querySelector('h1').innerText = 'this is now an official header'
