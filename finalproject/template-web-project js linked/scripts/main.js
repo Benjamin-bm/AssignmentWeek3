39e9628d1d95b556720304b49e991296490c4305
Benjamin-bm <112786874+Benjamin-bm@users.noreply.github.com>
2022-12-18 00:59:33 -0500

    .

--- finalproject/template-web-project js linked/scripts/main.js
+++ finalproject/template-web-project js linked/scripts/main.js
@@ -1,5 +1,31 @@
 //open this in devtools and see the result
 
+let xoff = 0.0;
+
+
+/* function setup() {
+  canvas = createCanvas(2000, 2000);
+  canvas.position(5000,5000);
+
+}
+
+function draw() {
+  clear();
+  background('rgba(255,255,255, 0)');
+  xoff = xoff + 0.001;
+  let n = noise(xoff) * 800;
+
+  strokeWeight(3)
+  line(1750, 1000, 1750, n);
+  line(35, 65, n, 65);
+  line(700, 500, 700, n)
+  line(n, 1000, 1300, 1000); 
+ 
+  fill (n,50,100,n)
+  /*circle(50, 500, 100);
+  circle (50, 600,100);
+} */ 
+
 console.log('this working!!')
 
 let div=document.createElement('div'); //create element
@@ -15,7 +41,7 @@ div.classList.add('btn-click');
 div.classList.remove('btn');
 
 div.innerText='dude whats up';
-document.querySelector('h1').innerText='Assignment week 11'
+document.querySelector('h1').innerText='A DIGITAL TOOLBOX'
 
 //select element to reference
 let header = document.querySelector('h1');
@@ -33,12 +59,10 @@ document.querySelector('.btn-click').innerHTML='<div class="test-outer"><div cla
 //let's create a new element for fun
 let newDiv = document.createElement('div');
 newDiv.classList.add('clickme');
-newDiv.innerText = 'Try to hover over me!';
+newDiv.innerText = 'WELCOME INSIDE';
 newDiv.style['font-size'] = '30px';
 newDiv.style.border = '1px solid black';
 document.querySelector('.container').appendChild(newDiv);
-
-let colors = ['red', 'blue', 'purple', 'green', 'black', 'yellow'];
 
 newDiv.addEventListener('mouseover', function(){
     console.log('is this workin');
@@ -52,11 +76,32 @@ let arr = ['red', 'green', 'blue']
 arr.forEach(myFunction);
 
 function bigImg(x) {
-    x.style.height = "200px";
-    x.style.width = "200px";
+    x.style.height = "480px";
+    x.style.width = "480px";
   }
   
   function normalImg(x) {
     x.style.height = "32px";
     x.style.width = "32px";
   }
+
+  function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
+  }
+
+  var playbutton = document.getElementById("noun-play-button-5385060 1");
+playbutton.addEventListener('load', function(){
+    console.log(arrow)
+});
+
+document.addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementById("").innerHTML = "Hello World";
+}
+
+function pictureChange()
+{
+document.getElementById("theImage").src="assets/elements01/photo2.png";
+}
